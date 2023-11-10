2ad9a291f80f21c25dfcd19734f1aa8e05d9b4c0
zackproject <zackprojectapp@gmail.com>
2023-11-10 16:17:15 +0100

    ngFor housingLocationList & hide environments

--- src/app/environment.ts
+++
@@ -1,3 +0,0 @@
-export const environment = {
-    apiUrl: 'https://angular.io/assets/images/tutorials/faa/', // URL de desarrollo
-  };
---
+++ src/app/home/environments/environment.example.ts
@@ -0,0 +1,3 @@
+export const environment = {
+    apiUrl: 'URL PATH IMAGES', 
+  };

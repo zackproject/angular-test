4306936f6241deaab913fddcebf8f1efd1227eee
zackproject <zackprojectapp@gmail.com>
2023-11-10 20:55:20 +0100

    Link id for each details

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
-
+import { Component, inject } from '@angular/core';
+// configure detail
+import { CommonModule } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
+import { HousingService } from '../housing.service';
+import { HousingLocationComponent } from '../housing-location/housing-location.component';
 @Component({
   selector: 'app-details',
   templateUrl: './details.component.html',
   styleUrls: ['./details.component.css']
 })
 export class DetailsComponent {
-
+  //activate router :id
+  route: ActivatedRoute = inject(ActivatedRoute);
+    housingLocationId = -1;
+    constructor() {
+        this.housingLocationId = Number(this.route.snapshot.params['id']);
+    }
 }
--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -1,9 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { HousingLocation } from '../housingLocation';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-housing-location',
   standalone: true,
+  imports: [RouterModule],
   templateUrl: './housing-location.component.html',
   styleUrls: ['./housing-location.component.css']
 })

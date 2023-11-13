a7749f370516d67257ea8e8bf5729078b450a943
zackproject <zackprojectapp@gmail.com>
2023-11-13 18:00:40 +0100

    Form in details/:id

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -4,20 +4,42 @@ import { CommonModule } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
 import { HousingService } from '../housing.service';
 import { HousingLocation } from '../housingLocation';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 @Component({
+  //Added stand alone
+  standalone: true,
   selector: 'app-details',
   templateUrl: './details.component.html',
-  styleUrls: ['./details.component.css']
+  styleUrls: ['./details.component.css'],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule
+  ],
 })
 export class DetailsComponent {
   //activate router :id
   route: ActivatedRoute = inject(ActivatedRoute);
   housingService = inject(HousingService);
   housingLocation: HousingLocation | undefined;
+  // activa el uso de formularios
+  applyForm = new FormGroup({
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+    email: new FormControl('')
+  });
 
   constructor() {
     //devuelve la informacion de 'housingLocation' segun la 'id'
     const housingLocationId = Number(this.route.snapshot.params['id']);
     this.housingLocation = this.housingService.getHousingLocationById(housingLocationId);
   }
+  // uso de formularios
+  submitApplication() {
+    this.housingService.submitApplication(
+      this.applyForm.value.firstName ?? '',
+      this.applyForm.value.lastName ?? '',
+      this.applyForm.value.email ?? ''
+    );
+  }
+
 }
--- src/app/housing.service.ts
+++ src/app/housing.service.ts
@@ -121,4 +121,11 @@ export class HousingService {
   getHousingLocationById(id: number): HousingLocation | undefined {
     return this.housingLocationList.find(housingLocation => housingLocation.id === id);
   }
+
+  //Send data form
+  submitApplication(firstName: string, lastName: string, email: string) {
+    console.log(`Homes application received: firstName: ${firstName}, lastName: ${lastName}, email: ${email}.`);
+  }
 }
+
+
